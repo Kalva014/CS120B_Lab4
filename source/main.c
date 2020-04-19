f689313344eb825b71455a08eec0c819b1a008a8
Kalva014 <kalva014@ucr.edu>
2020-04-18 19:01:34 -0700

    Completed part 1

--- source/main.c
+++ source/main.c
@@ -1,8 +1,8 @@
 /*	Author: kennethalvarez
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+ *	Assignment: Lab #4  Exercise #1
+ *	Exercise Description: PB0 and PB1 each connect to an LED, and PB0's LED is initially on.
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -12,12 +12,58 @@
 #include "simAVRHeader.h"
 #endif
 
+enum States {Init, LedPressed1, LedPressed2} state;
+
+void Tick() {
+	switch(state) { //transitions
+		case Init:
+			state = LedPressed1;
+			break;
+		case LedPressed1:
+			if((PINA & 0x01) == 0x01) {
+				state = LedPressed2;
+			}
+			else {
+				state = LedPressed1;
+			}
+			break;
+		case LedPressed2:
+			if((PINA & 0x01) == 0x01){
+				state = LedPressed1;
+			}
+			else {
+				state = LedPressed2;
+			}
+			break;
+		default:
+			state = LedPressed1;
+			break; 
+	}
+	switch(state) { //actions
+		case Init:
+			PORTB = 0x00;
+			break;
+		case LedPressed1:
+			PORTB = 0x01;
+			break;
+		case LedPressed2:
+			PORTB = 0x02;
+			break;
+		default:
+			break;
+	}
+}
+
+
 int main(void) {
     /* Insert DDR and PORT initializations */
+        DDRA = 0x00; PORTA = 0x00;
+        DDRB = 0xFF; PORTB = 0x00;
 
     /* Insert your solution below */
     while (1) {
-
+	PORTB = 0x00;
+	Tick();
     }
     return 1;
 }
--- test/tests.gdb
+++ test/tests.gdb
@@ -27,18 +27,32 @@ echo ======================================================\n
 echo Running all tests..."\n\n
 
 # Example test:
-test "PINA: 0x00, PINB: 0x00 => PORTC: 0"
+#test "PINA: 0x00, PINB: 0x00 => PORTC: 0"
 # Set inputs
-setPINA 0x00
-setPINB 0x00
+#setPINA 0x00
+#setPINB 0x00
 # Continue for several ticks
-continue 2
+#continue 2
 # Set expect values
-expectPORTC 0
+#expectPORTC 0
 # Check pass/fail
-checkResult
+#checkResult
 
 # Add tests below
+test "PINA: 0x00 => PORTB: 0x01, state: PressLED1"
+set state = Init
+setPINA 0x00
+continue 2
+expectPORTB 0x01
+checkResult
+
+test "PINA: 0x01 => PORTB: 0x02, state: PressLED2"
+set state = Init
+setPINA 0x01
+continue 2
+expectPORTB 0x02
+checkResult
+
 
 # Report on how many tests passed/tests ran
 set $passed=$tests-$failed
